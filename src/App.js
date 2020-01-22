803e6991f81351f70d92777a80f07e1f47f18ddd
Lars Karbo <mail@larskarbo.no>
2020-01-22 17:25:52 +0100

    responsive

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import d from "./initialChartData.json";
 import moment from "moment";
 import ColorScheme from "color-scheme";
 import Color from "color";
-import "react-input-range/lib/css/index.css"
-import InputRange from 'react-input-range';
-
+import "react-input-range/lib/css/index.css";
+import InputRange from "react-input-range";
+import { Container, Row, Col } from 'reactstrap';
 import Slider, { Range } from "rc-slider";
 const colors = [
   "B84D00",
@@ -22,7 +22,8 @@ const colors = [
 class App extends React.Component {
   state = {
     sod: 14,
-    cyclelength: 90
+    cyclelength: 90,
+    start:moment().hours(22).minutes(30)
   };
 
   componentDidMount() {
@@ -37,17 +38,19 @@ class App extends React.Component {
     window.napchart = napchart;
     this.chart = napchart;
     this.figma();
-    this.now();
+    this.draw();
 
     napchart.onUpdate = a => {
-      console.log('this.chart.data: ', this.chart.data);
-      const el = this.chart.data.elements.find(e=>e.id="sod")
-      console.log(el.start)
+      console.log("this.chart.data: ", this.chart.data);
+      const el = this.chart.data.elements.find(e => (e.id = "sod"));
+      console.log(el.start);
       this.setState({
-        start:  moment().startOf("day").add(el.start, 'minutes'),
+        start: moment()
+          .startOf("day")
+          .add(el.start, "minutes"),
         sod: this.chart.helpers.duration(el.start, el.end)
-      })
-    }
+      });
+    };
   }
 
   figma = () => {
@@ -88,7 +91,7 @@ class App extends React.Component {
 
   now = () => {
     // Your moment
-    var mmt = moment().hours(23);
+    var mmt = moment();
     this.state.start = mmt;
     this.draw();
   };
@@ -113,16 +116,22 @@ class App extends React.Component {
     }
 
     const fallAsleepTime = this.state.sod + bedTime;
-    console.log('this.state.sod: ', this.state.sod);
+    console.log("this.state.sod: ", this.state.sod);
 
     Array(7)
       .fill(0)
       .forEach((a, i) => {
         this.chart.createElement({
-          start: this.chart.helpers.limit(fallAsleepTime + this.state.cyclelength * i),
-          end: this.chart.helpers.limit(fallAsleepTime + this.state.cyclelength * (i + 1)),
+          start: this.chart.helpers.limit(
+            fallAsleepTime + this.state.cyclelength * i
+          ),
+          end: this.chart.helpers.limit(
+            fallAsleepTime + this.state.cyclelength * (i + 1)
+          ),
           color: "#" + colors[i],
-          text: this.chart.helpers.minutesToReadable(this.state.cyclelength * (i + 1))
+          text: this.chart.helpers.minutesToReadable(
+            this.state.cyclelength * (i + 1)
+          )
         });
       });
   };
@@ -130,52 +139,58 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div>
-          <div className="div">
-          <h2>When do you plan to go to bed?</h2>
-          <button onClick={this.now}>Now!</button>
-          (drag chart to get other values)
-          </div>
-          <div className="div">
-          <h2>How long time do you spend falling asleep?</h2>
-          <InputRange
-            minValue={0}
-            maxValue={120}
-            value={this.state.sod}
-            onChange={a => {
-              this.setState({
-                sod:a*1
-              }, ()=> this.draw())
-
-            }}
-          />
-          </div>
-          <div className="div">
-          <h2>Length of sleep cycles (normal: 90 minutes)</h2>
-          <InputRange
-            minValue={70}
-            maxValue={110}
-            value={this.state.cyclelength}
-            onChange={a => {
-              this.setState({
-                cyclelength:a*1
-              }, ()=> this.draw())
-
-            }}
-          />
-          </div>
-
-          <div className="div">
-          <h3>Look at the chart to see when it is smart to wake up</h3>
+        <div >
+          <div style={{ paddingTop: 25 }}>
+            <canvas width={800} height={800} ref={c => (this.c = c)}>
+              A chart
+            </canvas>
           </div>
         </div>
+
         <div>
-          <div style={{paddingTop: 25}}><canvas width={800} height={800} ref={c => (this.c = c)}>
-            A chart
-          </canvas>
+          <div className="div">
+            <h2>When do you plan to go to bed?</h2>
+            <button onClick={this.now}>Now!</button>
+            (drag chart to get other values)
           </div>
-          
+          <div className="div">
+            <h2>How long time do you spend falling asleep?</h2>
+            <InputRange
+              minValue={0}
+              maxValue={120}
+              value={this.state.sod}
+              onChange={a => {
+                this.setState(
+                  {
+                    sod: a * 1
+                  },
+                  () => this.draw()
+                );
+              }}
+            />
+          </div>
+          <div className="div">
+            <h2>Length of sleep cycles (normal: 90 minutes)</h2>
+            <InputRange
+              minValue={70}
+              maxValue={110}
+              value={this.state.cyclelength}
+              onChange={a => {
+                this.setState(
+                  {
+                    cyclelength: a * 1
+                  },
+                  () => this.draw()
+                );
+              }}
+            />
+          </div>
+
+          <div className="div">
+            <h3>Look at the chart to see when it is smart to wake up</h3>
+          </div>
         </div>
+        
       </div>
     );
   }
