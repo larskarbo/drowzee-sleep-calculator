0b4f65e171b5712a3412ce3386c080416823d8bb
Lars Karbo <mail@larskarbo.no>
2020-01-22 18:00:20 +0100

    Updates

---
+++ precache-manifest.b733d88f781df46a59de3029155b660e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c3f3121f74540abbc689e3639a4491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12e7df4a93e1c6aac64b",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "12e7df4a93e1c6aac64b",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/main.js.LICENSE"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
